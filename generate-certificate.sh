84bbcd23eec246d07702dbdadc0606e4b0253967
Amr Marey <amr.marey@msn.com>
2024-09-15 22:17:36 +0300

    fix certificate generation

--- generate-certificate.sh
+++ generate-certificate.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
 openssl req -x509 -newkey rsa:4096 -keyout ./cert/key.pem -out ./cert/cert.pem -sha256 -days 3650 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=*.almarey.work"
+
+
